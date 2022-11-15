97c53beef42f9407a4dbf83166edcc0f609f43e3
Martin Blanckaert <57911418+Martin-blckrt@users.noreply.github.com>
2022-11-14 20:56:16 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', event => {
         if (!navbarCollapsible) {
             return;
         }
-        if (window.scrollY === 0 && window.location.href === "index.html") {
+        if (window.scrollY === 0 && window.location.href === "/") {
             navbarCollapsible.classList.remove('navbar-shrink')
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
